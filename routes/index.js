30880050d4733a2d53fd9d17ff6e6aff3c1a2a5e
EchoS3 <h336699ss@163.com>
2021-01-22 17:44:44 +0800

    登录，首页，注册完成

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/index', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//登录路由配置
+router.get('/login', function(req, res) {
+  res.render('login', { });
+});
+//注册路由配置
+router.get('/zhuce', function(req, res) {
+  res.render('zhuce', { });
+});
 module.exports = router;
