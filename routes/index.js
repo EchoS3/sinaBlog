622de493d58d6c20b95bddf7712613b9ec300ecb
EchoS3 <h336699ss@163.com>
2021-01-23 13:06:56 +0800

    基础页面完成

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ router.get('/index', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//导航路由配置
+router.get('/details', function(req, res) {
+  res.render('details', { });
+});
 //登录路由配置
 router.get('/login', function(req, res) {
   res.render('login', { });
@@ -13,5 +17,9 @@ router.get('/login', function(req, res) {
 //注册路由配置
 router.get('/zhuce', function(req, res) {
   res.render('zhuce', { });
+});
+//写博客页面路由配置
+router.get('/write', function(req, res) {
+  res.render('write', { });
 });
 module.exports = router;
